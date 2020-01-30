2519333e71caad4f4c015fc6fdc5682f42f63427
Carlos Abdalla <carlos.abdalla@msdev.com.br>
2020-01-30 16:37:49 -0300

    webpack is rocks

---
+++ webpack.dev.ts
@@ -0,0 +1,31 @@
+import * as webpack from "webpack";
+import * as HtmlWebPackPlugin from "html-webpack-plugin";
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html"
+});
+
+const config: webpack.Configuration = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  module: {
+    rules: [
+      { 
+        test: /\.tsx?$/, 
+        loader: "ts-loader" 
+      }
+    ]
+  },
+  plugins: [htmlPlugin],
+  
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  }
+};
+
+export default config;
