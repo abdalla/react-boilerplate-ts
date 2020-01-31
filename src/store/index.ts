2c1442f2f41292ff2e68b778e176494a913457ae
Carlos Abdalla <carlos.abdalla@msdev.com.br>
2020-01-31 18:37:21 -0300

    fix: store is no longer using path for index
    store is no longer using path for index

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,11 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from '@reducers/index';
+import rootReducer from '../reducers';
 
 export type AppState = ReturnType<typeof rootReducer>;
 
 export default function configureStore() {
-  console.log('.....');
-  console.log(process.env.NODE_ENV);
-  console.log('.....');
-
   const middlewares = [thunkMiddleware];
   const middleWareEnhancer = applyMiddleware(...middlewares);
 
